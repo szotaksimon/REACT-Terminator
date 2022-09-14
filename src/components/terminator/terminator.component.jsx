27314b4c528ab7f1e950cf3062f5783440cb0c70
szotaksimon <szotak.simon16@gmail.com>
2022-09-14 16:38:29 +0200

    Terminátorok létrehozása és megjelenítése

---
+++ src/components/terminator/terminator.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Terminator = () => {
+  return (
+    <div className='bg-light-blue dib br3 pa3 ma2 grow'>
+      <img src="https://robohash.org/t/t?size=180x180" alt="terminátor" />
+      <div>
+        <h2>T800</h2>
+        <p>SN: 32673567</p>
+      </div>
+    </div>
+  );
+};
+export default Terminator;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Terminator from './components/terminator/terminator.component';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Terminator />
+  <div>
+    <Terminator />
+    <Terminator />
+    <Terminator />
+
+  </div>
 );
 
 // ReactDOM.render(<Hello />, document.getElementById("root"));
