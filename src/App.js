a0b7f2663985641b34b486b58fd7126988e49a5d
szotaksimon <szotak.simon16@gmail.com>
2022-09-20 16:26:55 +0200

    Api használata

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import TerminatorList from './components/terminator-list/terminator-list.component';
-import { models } from './models.js';
 import SearchBox from './components/searchbox/searchbox.component';
 import './App.css';
 
@@ -8,7 +7,7 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            models: models,
+            models: [],
             searchfield: "",
         }
     }
@@ -17,17 +16,30 @@ class App extends Component {
         this.setState({ searchfield: event.target.value });
     }
 
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((response) => response.json())
+        .then((users) => this.setState({models: users}));
+    }
+
     render() {
         const filteredModels = this.state.models.filter((model) => {
             return model.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
         })
+
+        if(this.state.models.length === 0) {
+            return <h1>Betöltés folyamatban...</h1>
+        }
+
+
         return (
             <div className="tc">
-                <h1 id="header-text">Terminator modellek</h1>
+                <h1>Terminator modellek</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <TerminatorList models={filteredModels} />
             </div>
         );
+
     }
 }
 
--- src/components/terminator-list/terminator-list.component.jsx
+++ src/components/terminator-list/terminator-list.component.jsx
@@ -10,7 +10,7 @@ const TerminatorList = ({ models }) => {
             key={model.id}
             id={model.id}
             name={model.name}
-            serialNumber={model.serialNumber}
+            serialNumber={model.phone}
           />
         );
       })}
