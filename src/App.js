beb4359e9fc489137d4419f89fa7e84b34896a04
szotaksimon <szotak.simon16@gmail.com>
2022-09-20 16:55:15 +0200

    Terminátor projekt hookokkal + számláló-gomb

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import TerminatorList from './components/terminator-list/terminator-list.component';
 import SearchBox from './components/searchbox/searchbox.component';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            models: [],
-            searchfield: "",
-        }
+function App() {
+    const [models, setModels] = useState([]);
+    const [searchfield, setSearchfield] = useState("");
+    const [szamlalo, setSzamlalo] = useState(0);
+
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value });
-    }
-
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => response.json())
-        .then((users) => this.setState({models: users}));
-    }
-
-    render() {
-        const filteredModels = this.state.models.filter((model) => {
-            return model.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
-        })
-
-        if(this.state.models.length === 0) {
-            return <h1>Betöltés folyamatban...</h1>
-        }
-
-
-        return (
-            <div className="tc">
-                <h1>Terminator modellek</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <TerminatorList models={filteredModels} />
-            </div>
+    useEffect(
+        () => {
+        console.log(szamlalo);
+            fetch("https://jsonplaceholder.typicode.com/users")
+                .then((response) => response.json())
+                .then((users) => setModels(users))}, 
+        [szamlalo]
         );
 
+
+
+
+    const filteredModels = models.filter((model) => {
+        return model.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+    })
+
+    if (models.length === 0) {
+        return <h1>Betöltés folyamatban...</h1>
     }
+
+
+    return (
+        <div className="tc">
+            <h1>Terminator modellek</h1>
+            <button onClick={() => setSzamlalo(szamlalo + 1)}>Kattints ide!</button>
+            <SearchBox searchChange={onSearchChange} />
+            <TerminatorList models={filteredModels} />
+        </div>
+    );
+
 }
+
 
 export default App;
